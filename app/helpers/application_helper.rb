63f43fd0f076f081c1a059c8ab804fe653d23294
mostlyclicks <carlos@mostlyclicks.com>
2014-01-12 22:20:53 -0600

    add bootstrap nav - disable bootstrap.min.js to work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def main_nav
+  	presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+  	presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
+    presenter.list_tag = "ul class=\"nav navbar-nav\""
+  	presenter.max_depth = 0
+  	presenter
+  end
 end
