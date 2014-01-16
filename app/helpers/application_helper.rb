27ca7a35dc77b91f9cf0aa12dfa0e3a9eab23c79
Carlos Interone <carlos@mostlyclicks.com>
2014-01-15 22:33:10 -0600

    create layer layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def main_nav
   	presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
-  	presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
+  	presenter.menu_tag = "div class=\"collapse navbar-collapse navbar-inverse\" id=\"main-nav-collapse\""
     presenter.list_tag = "ul class=\"nav navbar-nav\""
   	presenter.max_depth = 0
   	presenter
