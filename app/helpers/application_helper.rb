a36e0c700cb40031cdf949b8af07ebb5fa5915c4
Carlos Interone <carlos@mostlyclicks.com>
2014-01-29 20:48:50 -0600

    add nav options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
   	presenter.max_depth = 0
   	presenter
   end
+
+  def main_nav_nobg
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+    presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
+    presenter.list_tag = "ul class=\"nav navbar-nav pull-right pull-left-xs\""
+    presenter.max_depth = 0
+    presenter
+  end
+
+  def main_navbar_default
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+    presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
+    presenter.list_tag = "ul class=\"nav navbar-nav\""
+    presenter.max_depth = 0
+    presenter
+  end
 end
