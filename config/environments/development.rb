0cf45064025e6aeca36fd9f2da31bc0a374000c7
Carlos Interone <carlos@mostlyclicks.com>
2014-02-17 11:45:03 -0600

    precompile assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ Virq10Work::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 end
